f52586a34672a28df17e34bec66766623df80b90
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-08 11:07:51 -0600

    Heroku Update

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import Navigation from './navigation';
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  };
+
+  handleclick() {
+    if(!this.state.active) {
+      this.setState({active: true})
+    } else {
+      this.setState({active: false})
+    }
+    };
+
+  renderNavigation = function() {
+    if(this.state.active) {
+      console.log('ACTIVE')
+      return (
+        <Navigation />
+      )
+  }
+  }
+
+
   render() {
     return (
       <div className="wrapper">
             <div className="header">
-                <i className="fas fa-bars icon"></i>
+                <i className="fas fa-bars icon" onClick={()=> this.handleclick() }></i>
+                {this.renderNavigation()}
             </div>
             <div className="content">
                 {this.props.children}
---
+++ src/components/navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div className="navigation-box">
+                
+            </div>
+        )
+    }
+}
+
+export default Navigation;
