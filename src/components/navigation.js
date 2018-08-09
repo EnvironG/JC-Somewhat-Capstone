5ef162fc7ffe494cf4379bce557d11cc01ea6d4f
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-09 09:32:27 -0600

    Navigation Styles

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,8 +6,9 @@ class Navigation extends Component {
     render() {
         return (
                 <div id="box" className="navigation-box">
-                    <Link to="/side" onClick="window.location.reload()" className="navigation-box__link1"><div>Side</div></Link>
-                    <Link to="/filler" onClick="window.location.reload()" className="navigation-box__link2"><div>Filler</div></Link>
+                    <Link to="/" onClick="window.location.reload()" className="navigation-box__link1"><div>Main</div></Link>
+                    <Link to="/side" onClick="window.location.reload()" className="navigation-box__link2"><div>Side</div></Link>
+                    <Link to="/filler" onClick="window.location.reload()" className="navigation-box__link3"><div>Filler</div></Link>
                 </div>
         )
     }
